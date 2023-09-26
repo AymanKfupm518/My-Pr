fa9d5a5f3cabe09483cd69d152c1bb5943da7245
AymanKfupm518 <146083556+AymanKfupm518@users.noreply.github.com>
2023-09-26 14:54:12 +0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println("bye World!");
+        System.out.println(generateGroups(readFile("Student List.txt"),4));
+
+    }
+    public static ArrayList<String> readFile(String filename){
+        ArrayList arr = new ArrayList<>();
+        try {
+            File f = new File(filename);
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine()){https://github.com/Hussain18536/SWE206Lab3/blob/main/.idea/.gitignore
+                arr.add(sc.nextLine());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println(e.getMessage());
+        }
+        return arr;
+
+    }
+    public static ArrayList<ArrayList<String>> generateGroups(ArrayList<String> studentList, int  numOfGroups){
+        ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>();
+
+        Collections.shuffle(studentList);
+        for(int i = 0 ; i< numOfGroups; i++){
+            groups.add(new ArrayList<String>());
+        }
+
+        for(int i = 0  ; i < studentList.size(); i++ ){
+            groups.get(i%numOfGroups).add(studentList.get(i));
+        }
+        return groups;
+    }
+}
